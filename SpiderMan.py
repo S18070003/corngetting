6e219f4c8c8fdcc6731f314a9a5b84eed9184652
Gaoxun <823931104@qq.com>
2020-01-26 23:00:20 +0800

    123

--- SpiderMan.py
+++ SpiderMan.py
@@ -22,10 +22,14 @@ class SpiderMan(object):
                 # 从URL管理器获取新的url
                 new_url = self.manager.get_new_url()
                 # HTML下载器下载网页
+                print("1")
                 html = self.downloader.download(new_url)
+                print("2")
                 new_urls, data = self.parser.parser(new_url, html)
+                print("3")
                 # 将抽取的url添加到URL管理器中
                 self.manager.add_new_urls(new_urls)
+                print("4")
                 # 数据存储器存储文件
                 self.output.store_data(data)
                 print("已经抓取%s个链接" % self.manager.old_url_size())
@@ -38,3 +42,7 @@ class SpiderMan(object):
 if __name__ == "__main__":
     spider_man = SpiderMan()
     spider_man.crawl("http://baike.baidu.com/view/284853.htm")
+
+    '''
+    对于百度，有放爬虫机制，需要伪装成浏览器，这里暂时没有试试
+    '''
